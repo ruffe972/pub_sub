7503160cb2116c554cfa6ce1137cab9b4498abd5
Ivan Ershov <ivanershov51@gmail.com>
2020-12-12 02:29:11 +0300

    Gradle: Groovy -> Kotlin

---
+++ subscriber/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("org.springframework.boot") version "2.4.1"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("java")
+}
+
+group = "ruffe972"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    runtimeOnly("org.postgresql:postgresql")
+}
