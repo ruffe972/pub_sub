33ab98ea96ef06a34a05424c1cdbc2b9a15ec5b7
Ivan Ershov <ivanershov51@gmail.com>
2020-12-11 18:40:09 +0300

    Create starter Spring Boot apps

---
+++ publisher/src/test/java/ruffe972/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package ruffe972.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
