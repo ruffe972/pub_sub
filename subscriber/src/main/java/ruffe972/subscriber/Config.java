947563b5650aebb4221f403059dc31a5e9ca5a04
Ivan Ershov <ivanershov51@gmail.com>
2020-12-18 17:29:56 +0300

    Change database password and make it private

--- subscriber/src/main/java/ruffe972/subscriber/Config.java
+++ subscriber/src/main/java/ruffe972/subscriber/Config.java
@@ -1,5 +1,6 @@
 package ruffe972.subscriber;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,12 +12,12 @@ import javax.sql.DataSource;
 @Configuration
 public class Config {
     @Bean
-    DataSource dataSource() {
+    DataSource dataSource(@Value("${database_password}") String password) {
         var dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432/messaging");
         dataSource.setUsername("postgres");
-        dataSource.setPassword("1");
+        dataSource.setPassword(password);
         return dataSource;
     }
 
